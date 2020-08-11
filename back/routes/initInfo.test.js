a1aff990699b2baf79a06c81693bccfc454e939b
WHO-A-U <djfxl46@naver.com>
2020-08-11 20:13:54 +0900

    3가지 section 에 대하여 init 함수 생성

--- back/routes/initInfo.test.js
+++ back/routes/initInfo.test.js
@@ -9,13 +9,33 @@ const getHtml = async (url) => {
     console.error(error);
   }
 };
+const urls = [];
+for (let i = 1; i <= 3; i++) {
+  urls.push({
+    url: `http://www.hongik.ac.kr/front/boardlist.do?currentPage=${i}&menuGubun=1&siteGubun=1&bbsConfigFK=2&searchField=ALL&searchValue=&searchLowItem=ALL`,
+    section: 1,
+  });
+}
+for (let i = 1; i <= 3; i++) {
+  urls.push({
+    url: `http://www.hongik.ac.kr/front/boardlist.do?currentPage=${i}&menuGubun=1&siteGubun=1&bbsConfigFK=3&searchField=ALL&searchValue=&searchLowItem=ALL`,
+    section: 2,
+  });
+}
+urls.push({
+  url: `http://www.hongik.ac.kr/front/boardlist.do?bbsConfigFK=54&siteGubun=1&menuGubun=1`,
+  section: 3,
+});
+
 exports.initTable = (req, res, next) => {
   const info = async () => {
     let all = [];
-    for (let i = 1; i <= 4; i++) {
-      await getHtml(
-        `http://www.hongik.ac.kr/front/boardlist.do?currentPage=${i}&menuGubun=1&siteGubun=1&bbsConfigFK=2&searchField=ALL&searchValue=&searchLowItem=ALL`
-      ).then((html) => {
+
+    // urls.forEach( (x) => {
+
+    for (let k = 0; k < urls.length; k++) {
+      console.log(urls[k]);
+      await getHtml(urls[k].url).then((html) => {
         let ulList = [];
         const $ = cheerio.load(html.data);
         const $bodyList = $(
@@ -26,21 +46,24 @@ exports.initTable = (req, res, next) => {
             title: $(this).find('div.subject span').text(),
             url: 'www.hongik.ac.kr' + $(this).find('a').attr('href'),
             date: $(this).find('td:nth-child(5)').text(),
+            section: urls[k].section,
           };
         });
         all = all.concat(ulList);
+        // console.log(all);
         const data = ulList.filter((n) => n.title);
         return data;
       });
       // .then((res) => log(res));
     }
+    console.log(all);
     await all.forEach((x) => {
       db.Information.findOrCreate({
         where: {
           title: x.title,
           url: x.url,
           date: x.date,
-          section: 1,
+          section: parseInt(x.section, 10),
         },
       });
     });
