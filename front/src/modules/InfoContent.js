004faf4a4e86cba86a931704510aa0691453fd79
WHO-A-U <djfxl46@naver.com>
2020-08-25 02:44:47 +0900

    redux structure

--- front/src/modules/InfoContent.js
+++ front/src/modules/InfoContent.js
@@ -1,8 +1,48 @@
+import { createAciton, handleAciton } from 'react-redux';
+import * as api from '../library/api';
+
 export const initialState = {
-  isInfoUpdate: false,
   info: {},
+  loading: false,
+  error: false,
 };
 
-export const INFO_REQUEST = 'INFO_REQUEST';
-export const INFO_SUCCESS = 'INFO_SUCCESS';
-export const INFO_FAILURE = 'INFO_FAILURE';
+const GET_INFO_REQUEST = 'GET_INFO_REQUEST';
+const GET_INFO_SUCCESS = 'GET_INFO_SUCCESS';
+const GET_INFO_FAILURE = 'GET_INFO_FAILURE';
+
+export const getInfo = () => async (dispatch) => {
+  dispatch({ type: GET_INFO_REQUEST });
+  try {
+    const res = await api.getInfo();
+    dispatch({
+      type: GET_INFO_SUCCESS,
+      action: res.data,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_INFO_FAILURE,
+      action: e,
+      error: true,
+    });
+    throw e;
+  }
+};
+
+const InfoContent = handleAciton({
+  [GET_INFO_REQUEST]: (state) => ({
+    ...state,
+    loading: true,
+  }),
+  [GET_INFO_SUCCESS]: (state, action) => ({
+    ...state,
+    info: action.data,
+    loading: false,
+  }),
+  [GET_INFO_FAILURE]: (state, action) => ({
+    ...state,
+    error: true,
+  }),
+});
+
+export default InfoContent;
--- front/src/modules/MyHistory.js
+++ front/src/modules/MyHistory.js
@@ -1,10 +1,41 @@
+import { createAciton, handleAction } from 'react-redux';
+import * as api from '../library/api';
+
 export const initialState = {
   islogin: false,
   loginFailure: false,
   admissionYear: null,
   history: {},
+  classnet: null,
+  classnetPass: null,
 };
 
-export const HISTORY_REQUEST = 'HISTORY_REQUEST';
-export const HISTORY_SUCCESS = 'HISTORY_SUCCESS';
-export const HISTORY_FAILURE = 'HISTORY_FAILURE';
+export const GET_HISTORY_REQUEST = 'GET_HISTORY_REQUEST';
+export const GET_HISTORY_SUCCESS = 'GET_HISTORY_SUCCESS';
+export const GET_HISTORY_FAILURE = 'GET_HISTORY_FAILURE';
+
+export const getHistory = () => async (dispatch) => {
+  dispatch({ type: GET_HISTORY_REQUEST });
+  try {
+    const res = await api.getUsers();
+    dispatch({
+      type: GET_HISTORY_SUCCESS,
+      data: res.data,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_HISTORY_FAILURE,
+      data: e,
+      error: true,
+    });
+    throw e;
+  }
+};
+
+const MyHistory = handleAction({
+  [GET_HISTORY_REQUEST]: (state) => ({}),
+  [GET_HISTORY_SUCCESS]: (state) => ({}),
+  [GET_HISTORY_FAILURE]: (state) => ({}),
+});
+
+export default MyHistory;
