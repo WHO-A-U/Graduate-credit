99f4705763ede7cfb169c7b790e365d634ad144e
WHO-A-U <djfxl46@naver.com>
2020-08-26 21:30:49 +0900

    redux 와 component 연결한 container

--- front/src/container/InfoTabsContainer.js
+++ front/src/container/InfoTabsContainer.js
@@ -3,37 +3,31 @@ import { Tabs } from 'antd';
 import InfoContent from '../components/InfoContent';
 import InfoTabs from '../components/InfoTabs';
 import { connect, useSelector, useDispatch } from 'react-redux';
-import { getInfo } from '../modules/InfoContent';
+import { getInfo } from '../modules/infoContent';
 const { TabPane } = Tabs;
 
-const InfoTabsContainer = ({ info, loading, error, getInfo }) => {
-    const info = useSelector(state=>state.info)
-    const loading = useSelector(state=>state.loading)
-    const error = useSelector(state=>state.error)
-    const dispatch = useDispatch();
+const InfoTabsContainer = ({ setCurInfofn, info, loading, error, getInfo }) => {
+  // const info = useSelector((state) => state.info);
+  // const loading = useSelector((state) => state.loading);
+  // const error = useSelector((state) => state.error);
+  // const dispatch = useDispatch();
   useEffect(() => {
-    dispatch({type:})
-  }, []);
+    getInfo();
+    console.log(info);
+    console.log(loading);
+    console.log(error);
+    console.log(getInfo);
+  }, [getInfo]);
 
-  return Object.keys(info) == 0 ? (
+  return !info ? (
     <div>로딩중....</div>
   ) : (
     <>
-      {/* <Tabs defaultActiveKey="1">
-          <TabPane tab="일반 공지" key="1">
-            <InfoContent Contentlist={info[1]}></InfoContent>
-          </TabPane>
-          <TabPane tab="학생 공지" key="2">
-            <InfoContent Contentlist={info[2]}></InfoContent>
-          </TabPane>
-          <TabPane tab="컴퓨터공학과 공지" key="3">
-            <InfoContent Contentlist={info[3]}></InfoContent>
-          </TabPane>
-        </Tabs> */}
       <InfoTabs
         studentContentList={info[1]}
         normalContentList={info[2]}
         computerContentList={info[3]}
+        setCurInfofn={setCurInfofn}
       ></InfoTabs>
     </>
   );
@@ -42,10 +36,10 @@ const InfoTabsContainer = ({ info, loading, error, getInfo }) => {
 // export default InfoTabs;
 
 export default connect(
-  ({ InfoTabs }) => ({
-    info: InfoTabs.info,
-    loading: InfoTabs.loading,
-    error: InfoTabs.error,
+  ({ infoContent }) => ({
+    info: infoContent.info,
+    loading: infoContent.loading,
+    error: infoContent.error,
   }),
   {
     getInfo,
