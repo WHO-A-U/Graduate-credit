113b2dca5ee83b41fc999f6d0c267ee0d593b67f
WHO-A-U <djfxl46@naver.com>
2020-08-26 21:23:11 +0900

    json 파일 형식 변형

--- back/routes/information.js
+++ back/routes/information.js
@@ -18,14 +18,17 @@ router.get('/', async (req, res, next) => {
     const firstInfo = await db.Information.findAll({
       where: { section: 1 },
       attributes: ['title', 'url', 'date'],
+      order: [['date', 'DESC']],
     });
     const secondInfo = await db.Information.findAll({
       where: { section: 2 },
       attributes: ['title', 'url', 'date'],
+      order: [['date', 'DESC']],
     });
     const thirdInfo = await db.Information.findAll({
       where: { section: 3 },
       attributes: ['title', 'url', 'date'],
+      order: [['date', 'DESC']],
     });
     return res.json({ 1: firstInfo, 2: secondInfo, 3: thirdInfo });
   } catch (e) {
