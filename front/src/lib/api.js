07dc7988d9f49c0dc2e75dc2995b95883443eebe
WHO-A-U <djfxl46@naver.com>
2020-08-26 21:30:20 +0900

    폴더 이름 변경

---
+++ front/src/lib/api.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const getInfo = () =>
+  axios.get('http://localhost:3065/api/information/');
+
+export const getHistory = (id, pw) => axios.get('url~~~~`');
--- front/src/library/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const getInfo = () => {
-  axios.get('http://localhost:3065/api/information/');
-};
-export const getHistory = (id, pw) => {
-  axios.get('url~~~~`');
-};
