91b47e8c48e65383ca0b4d26699ed57db31e2325
WHO-A-U <djfxl46@naver.com>
2020-08-30 01:08:24 +0900

    getHistory api  post method 정의

--- front/src/lib/api.js
+++ front/src/lib/api.js
@@ -3,4 +3,21 @@ import axios from 'axios';
 export const getInfo = () =>
   axios.get('http://localhost:3065/api/information/');
 
-export const getHistory = (id, pw) => axios.get('url~~~~`');
+// export const getHistory = (id, pw) => {
+//   return axios.get('http://localhost:3065/api/myHistory');
+// };
+export const getHistory = (id, pw) => {
+  console.log('여기는 깊숙한 곳~~~~');
+  console.log(id);
+  console.log(pw);
+  console.log('여기는 깊숙한 곳~~~~');
+  return axios({
+    method: 'post',
+    url: 'http://localhost:3065/api/myHistory',
+    headers: {},
+    data: {
+      classnet: id,
+      classnetPass: pw,
+    },
+  });
+};
