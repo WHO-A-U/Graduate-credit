047c7ff5c0d2e295b2af012b11527c2fb2b69864
zmrdltl <meenseek5929@naver.com>
2020-09-01 00:31:43 +0900

    conflict 수정

--- front/src/components/GraduateInfo.js
+++ front/src/components/GraduateInfo.js
@@ -1,20 +1,17 @@
-import React, { useState } from 'react';
-import { Table, Tag, Space, Row, Col } from 'antd';
-import { useSelector } from 'react-redux';
-import MyHistory from '../components/MyHistory';
+import React, { useState } from "react";
+import { Table, Tag, Space, Row, Col } from "antd";
+import { useSelector } from "react-redux";
+import MyHistory from "../components/MyHistory";
 
-import MyGraduateInfo10To15 from './MyGraduateInfo10-15';
-import MyGraduateInfo16To18 from './MyGraduateInfo16-18';
-import MyGraduateInfo19 from './MyGraduateInfo19';
-import MyGraduateInfo20 from './MyGraduateInfo20';
-import useSelection from 'antd/lib/table/hooks/useSelection';
+import MyGraduateInfo10To15 from "./MyGraduateInfo10-15";
+import MyGraduateInfo16To18 from "./MyGraduateInfo16-18";
+import MyGraduateInfo19 from "./MyGraduateInfo19";
+import MyGraduateInfo20 from "./MyGraduateInfo20";
 
-import DragonBall16_18 from './DragonBall16_18';
-
-const condition1015 = require('../lib/Condition10-15');
-const condition1618 = require('../lib/Condition16-18');
-const condition19 = require('../lib/Condition19');
-const condition20 = require('../lib/Condition20');
+const condition1015 = require("../lib/Condition10-15");
+const condition1618 = require("../lib/Condition16-18");
+const condition19 = require("../lib/Condition19");
+const condition20 = require("../lib/Condition20");
 
 const GraduateInfo = ({ history }) => {
   const graduateState10To15 = condition1015(history.subject, history.info);
@@ -24,12 +21,12 @@ const GraduateInfo = ({ history }) => {
   const myYear = parseInt(
     useSelector((state) => state.myHistory.admissionYear)
   );
-  console.log('10~15학생 졸업 state!!!', graduateState10To15);
-  console.log('16~18학생 졸업 state!!!', graduateState16To18);
-  console.log('19학생 졸업 state!!!', graduateState19);
-  console.log('20학생 졸업 state!!!', graduateState20);
-  console.log('내 입학년도다!!!!', myYear);
-  console.log('난 통짜 히스토리다!!!!', history);
+  console.log("10~15학생 졸업 state!!!", graduateState10To15);
+  console.log("16~18학생 졸업 state!!!", graduateState16To18);
+  console.log("19학생 졸업 state!!!", graduateState19);
+  console.log("20학생 졸업 state!!!", graduateState20);
+  console.log("내 입학년도다!!!!", myYear);
+  console.log("난 통짜 히스토리다!!!!", history);
   return (
     <>
       <div>
