d4d835bb8e47c83c93d979793021c25cabee5ed7
WHO-A-U <djfxl46@naver.com>
2020-09-01 00:18:03 +0900

    condition 수정

--- front/src/components/GraduateInfo.js
+++ front/src/components/GraduateInfo.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
-import { Table, Tag, Space, Row, Col } from "antd";
-import { useSelector } from "react-redux";
-import MyHistory from "../components/MyHistory";
+import React, { useState } from 'react';
+import { Table, Tag, Space, Row, Col } from 'antd';
+import { useSelector } from 'react-redux';
+import MyHistory from '../components/MyHistory';
 
-import MyGraduateInfo10To15 from "./MyGraduateInfo10-15";
-import MyGraduateInfo16To18 from "./MyGraduateInfo16-18";
-import MyGraduateInfo19 from "./MyGraduateInfo19";
-import MyGraduateInfo20 from "./MyGraduateInfo20";
-import useSelection from "antd/lib/table/hooks/useSelection";
+import MyGraduateInfo10To15 from './MyGraduateInfo10-15';
+import MyGraduateInfo16To18 from './MyGraduateInfo16-18';
+import MyGraduateInfo19 from './MyGraduateInfo19';
+import MyGraduateInfo20 from './MyGraduateInfo20';
+import useSelection from 'antd/lib/table/hooks/useSelection';
 
-const condition1015 = require("../lib/Condition10-15");
-const condition1618 = require("../lib/Condition16-18");
-const condition19 = require("../lib/Condition19");
-const condition20 = require("../lib/Condition20");
+import DragonBall16_18 from './DragonBall16_18';
+
+const condition1015 = require('../lib/Condition10-15');
+const condition1618 = require('../lib/Condition16-18');
+const condition19 = require('../lib/Condition19');
+const condition20 = require('../lib/Condition20');
 
 const GraduateInfo = ({ history }) => {
   const graduateState10To15 = condition1015(history.subject, history.info);
@@ -20,10 +22,10 @@ const GraduateInfo = ({ history }) => {
   const graduateState19 = condition19(history.subject, history.info);
   const graduateState20 = condition20(history.subject, history.info);
   const myYear = useSelector((state) => state.myHistory.admissionYear);
-  console.log("10~15학생 졸업 state!!!", graduateState10To15);
-  console.log("난 난2010~2015학생의 입학년도다!!!!", myYear);
+  console.log('10~15학생 졸업 state!!!', graduateState10To15);
+  console.log('난 난2010~2015학생의 입학년도다!!!!', myYear);
   console.log(typeof myYear);
-  console.log("난 통짜 히스토리다!!!!", history);
+  console.log('난 통짜 히스토리다!!!!', history);
   return (
     <>
       <div>
@@ -33,14 +35,14 @@ const GraduateInfo = ({ history }) => {
           </Col>
 
           <Col span={16}>
-            {myYear === "2015" ? (
+            {myYear === '2015' ? (
               <MyGraduateInfo10To15
                 graduateState={graduateState10To15}
                 myYear={myYear}
               />
-            ) : myYear === "2016" ? (
-              <MyGraduateInfo16To18 graduateState={graduateState16To18} />
-            ) : myYear === "2019" ? (
+            ) : myYear === '2016' ? (
+              <DragonBall16_18 obj={graduateState16To18} />
+            ) : myYear === '2019' ? (
               <MyGraduateInfo19 graduateState={graduateState19} />
             ) : (
               <MyGraduateInfo20 graduateState={graduateState20} />
