6fbe45e917129ab56b9d841ae1059db2146a9c3f
WHO-A-U <djfxl46@naver.com>
2020-08-31 19:29:36 +0900

    로딩시 Loading 효과 <Spin> 태그Loading.js 따로 놓을 필요 없음

--- front/src/components/GraduateForm.js
+++ front/src/components/GraduateForm.js
@@ -1,10 +1,9 @@
 import React, { useCallback, useEffect } from 'react';
-import { Form, Input, Button, Select } from 'antd';
+import { Form, Input, Button, Select, Spin } from 'antd';
 const { Option } = Select;
 import { useDispatch, connect } from 'react-redux';
 import { LOGIN_FAIL } from '../modules/myHistory';
 import { getHistory } from '../modules/myHistory';
-import Loading from './Loading';
 const layout = {
   labelCol: { span: 8 },
   wrapperCol: { span: 16 },
@@ -40,55 +39,57 @@ const GraduateForm = ({ isLogined, isLoading, loginFailure, getHistory }) => {
   });
 
   return (
-    <Form
-      form={form}
-      {...layout}
-      name="studentForm"
-      labelAlign="left"
-      initialValues={{ remember: true }}
-      onFinish={onSubmitForm}
-      size="middle"
-      style={formStyle}
-    >
-      <Form.Item
-        label="학번"
-        name="userid"
-        rules={[{ required: true, message: '클래스넷 학번을 입력해주세요!' }]}
+    <Spin spinning={isLoading} tip="Loading ... ">
+      <Form
+        form={form}
+        {...layout}
+        name="studentForm"
+        labelAlign="left"
+        initialValues={{ remember: true }}
+        onFinish={onSubmitForm}
+        size="middle"
+        style={formStyle}
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="학번"
+          name="userid"
+          rules={[{ required: true, message: '클래스넷 학번을 입력해주세요!' }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        className="mypwishere!!!"
-        label="Password"
-        name="password"
-        rules={[
-          { required: true, message: '클래스넷 비밀번호를 입력해주세요' },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-      <Form.Item
-        name="admissionYear"
-        label="admissionYear"
-        rules={[{ required: true, message: '필수선택사항 입니다' }]}
-      >
-        <Select placeholder="입학년도를 선택해주세요" allowClear>
-          <Option value="2015">2015</Option>
-          <Option value="2016">2016</Option>
-          <Option value="2018">2018</Option>
-        </Select>
-      </Form.Item>
-      {/* <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Form.Item
+          className="mypwishere!!!"
+          label="Password"
+          name="password"
+          rules={[
+            { required: true, message: '클래스넷 비밀번호를 입력해주세요' },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          name="admissionYear"
+          label="admissionYear"
+          rules={[{ required: true, message: '필수선택사항 입니다' }]}
+        >
+          <Select placeholder="입학년도를 선택해주세요" allowClear>
+            <Option value="2015">2015</Option>
+            <Option value="2016">2016</Option>
+            <Option value="2018">2018</Option>
+          </Select>
+        </Form.Item>
+        {/* <Form.Item {...tailLayout} name="remember" valuePropName="checked">
         <Checkbox>Remember me</Checkbox>
       </Form.Item> */}
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Spin>
   );
 };
 
