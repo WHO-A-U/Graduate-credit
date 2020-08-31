24fb7b9a1dfa5c00b3c34488c501c3084c16292b
zmrdltl <meenseek5929@naver.com>
2020-08-31 19:33:52 +0900

    Loading animation 임시 pull 받기

--- front/src/components/GraduateInfo.js
+++ front/src/components/GraduateInfo.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { Table, Tag, Space, Row, Col } from "antd";
+import { useSelector } from "react-redux";
 import MyHistory from "../components/MyHistory";
 
 import MyGraduateInfo10To15 from "./MyGraduateInfo10-15";
 import MyGraduateInfo16To18 from "./MyGraduateInfo16-18";
 import MyGraduateInfo19 from "./MyGraduateInfo19";
 import MyGraduateInfo20 from "./MyGraduateInfo20";
+import useSelection from "antd/lib/table/hooks/useSelection";
 
 const condition1015 = require("../lib/Condition10-15");
 const condition1618 = require("../lib/Condition16-18");
@@ -17,8 +19,10 @@ const GraduateInfo = ({ history }) => {
   const graduateState16To18 = condition1618(history.subject, history.info);
   const graduateState19 = condition19(history.subject, history.info);
   const graduateState20 = condition20(history.subject, history.info);
-  console.log("난그레듓!!!", graduateState10To15);
-  console.log("난 입학년도다!!!!", history.admissionYear);
+  const myYear = useSelector((state) => state.myHistory.admissionYear);
+  console.log("10~15학생 졸업 state!!!", graduateState10To15);
+  console.log("난 난2010~2015학생의 입학년도다!!!!", myYear);
+  console.log(typeof myYear);
   console.log("난 통짜 히스토리다!!!!", history);
   return (
     <>
@@ -29,8 +33,18 @@ const GraduateInfo = ({ history }) => {
           </Col>
 
           <Col span={16}>
-            {/* <MyGraduateInfo16To18 graduateState={graduateState} /> */}
-            <MyGraduateInfo10To15 graduateState={graduateState10To15} />
+            {myYear === "2015" ? (
+              <MyGraduateInfo10To15
+                graduateState={graduateState10To15}
+                myYear={myYear}
+              />
+            ) : myYear === "2016" ? (
+              <MyGraduateInfo16To18 graduateState={graduateState16To18} />
+            ) : myYear === "2019" ? (
+              <MyGraduateInfo19 graduateState={graduateState19} />
+            ) : (
+              <MyGraduateInfo20 graduateState={graduateState20} />
+            )}
           </Col>
         </Row>
       </div>
