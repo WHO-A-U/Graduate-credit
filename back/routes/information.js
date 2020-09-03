2cf22569433bbe9010e2e7e1733888a31a02f503
WHO-A-U <59171080+WHO-A-U@users.noreply.github.com>
2020-09-03 23:33:23 +0900

    getInfo 할떄 1~5 까지 json 호출 + 코로나 + 대외활동 공지 포함

--- back/routes/information.js
+++ back/routes/information.js
@@ -1,25 +1,41 @@
-const express = require("express");
-const db = require("../models");
+const express = require('express');
+const db = require('../models');
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const firstInfo = await db.Information.findAll({
       where: { section: 1 },
-      attributes: ["title", "url", "date"],
-      order: [["date", "DESC"]],
+      attributes: ['title', 'url', 'date'],
+      order: [['date', 'DESC']],
     });
     const secondInfo = await db.Information.findAll({
       where: { section: 2 },
-      attributes: ["title", "url", "date"],
-      order: [["date", "DESC"]],
+      attributes: ['title', 'url', 'date'],
+      order: [['date', 'DESC']],
     });
     const thirdInfo = await db.Information.findAll({
       where: { section: 3 },
-      attributes: ["title", "url", "date"],
-      order: [["date", "DESC"]],
+      attributes: ['title', 'url', 'date'],
+      order: [['date', 'DESC']],
     });
-    return res.json({ 1: firstInfo, 2: secondInfo, 3: thirdInfo });
+    const fourthInfo = await db.Information.findAll({
+      where: { section: 4 },
+      attributes: ['title', 'url', 'date'],
+      order: [['date', 'DESC']],
+    });
+    const FifthInfo = await db.Information.findAll({
+      where: { section: 5 },
+      attributes: ['title', 'url', 'date'],
+      order: [['date', 'DESC']],
+    });
+    return res.json({
+      1: firstInfo,
+      2: secondInfo,
+      3: thirdInfo,
+      4: fourthInfo,
+      5: FifthInfo,
+    });
   } catch (e) {
     console.error(e);
     return next(e);
