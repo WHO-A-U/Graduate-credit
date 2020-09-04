0eea7f46a2e6d4c7f986cdb3bbc2c15d4e6b1fb2
zmrdltl <meenseek5929@naver.com>
2020-09-04 14:52:24 +0900

    FAQ page 추가

---
+++ front/src/components/FAQ.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const FAQ = () => {
+  return (
+    <iframe
+      width="50%"
+      src="https://docs.google.com/forms/d/1OkqKWo-QNAqHdcljvwwxvBuynWIz5cj0vL_JXd44euQ/edit"
+      name="test"
+      id="test"
+      frameborder="1"
+      scrolling="yes"
+      align="left"
+    >
+      이 브라우저는 iframe을 지원하지 않습니다
+    </iframe>
+  );
+};
+
+export default FAQ;
