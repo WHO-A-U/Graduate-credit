bf82f5b3924bb415aa3a1afc5fbb4ce179881f65
zmrdltl <meenseek5929@naver.com>
2020-09-05 15:49:44 +0900

    pull from master

--- front/src/components/MyHistory.js
+++ front/src/components/MyHistory.js
@@ -3,9 +3,9 @@ import { Table, Tag } from "antd";
 import PropTypes from "prop-types";
 const columns = [
   {
-    title: '학수번호',
-    dataIndex: 'number',
-    key: 'number',
+    title: "학수번호",
+    dataIndex: "number",
+    key: "number",
     render: (text) => <a href="/#">{text}</a>,
   },
   {
@@ -30,7 +30,6 @@ const columns = [
     render: (text) => {
       if (text === "A+") {
         return <Tag color="purple">A+</Tag>;
-<<<<<<< HEAD
       } else if (text === "A0") {
         return <Tag color="geekblue">A0</Tag>;
       } else if (text === "B+") {
@@ -46,23 +45,6 @@ const columns = [
       } else if (text === "D0") {
         return <Tag color="magenta">D0</Tag>;
       } else if (text === "P") {
-=======
-      } else if (text === 'A0') {
-        return <Tag color="geekblue">A0</Tag>;
-      } else if (text === 'B+') {
-        return <Tag color="green">B+</Tag>;
-      } else if (text === 'B0') {
-        return <Tag color="lime">B0</Tag>;
-      } else if (text === 'C+') {
-        return <Tag color="orange">C+</Tag>;
-      } else if (text === 'C0') {
-        return <Tag color="volcano">C0</Tag>;
-      } else if (text === 'D+') {
-        return <Tag color="red">D+</Tag>;
-      } else if (text === 'D0') {
-        return <Tag color="magenta">D0</Tag>;
-      } else if (text === 'P') {
->>>>>>> a24eb0524628788cdd1bdfa079f80a8fc315e5c1
         return <Tag color="default">P</Tag>;
       }
     },
